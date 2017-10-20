1eaef61232888307a4a8e57707fa2c990b4db659
zlodiak <prozaik81-2@yandex.ru>
2017-10-21 01:18:23 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AgendaService } from './services/agenda.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+	private agendaObj: any[] = [];
+	private agenda: any[] = [];
+	private dates: string[] = [];
+  
+  constructor(private agendaService: AgendaService) { };
+
+	ngOnInit() {
+		this.getAgenda();
+	}  
+
+  private getAgenda(): void {
+    this.agendaService
+        .getAgenda()
+        .subscribe(data => {     
+        						let response = JSON.parse(data._body);
+        						//console.log(response); 
+
+        						this.dates = response['headers'].date.options;        					                                                         
+                    console.log(this.dates);                                                         
+
+										this.agendaObj = response.values;
+										//console.log(this.agendaObj);  
+
+                    let this_ = this;
+										Object.keys(this.agendaObj).map(function(val, key) {
+										  this_.agenda.push({
+										  	date: +this_.agendaObj[val].date,
+										  	start: this_.agendaObj[val].start,
+										  	end: this_.agendaObj[val].end,										  	
+										  	title: this_.agendaObj[val].title,
+										  	place: this_.agendaObj[val].place,
+										  	order: +this_.agendaObj[val].order
+										  });
+										});  
+										console.log(this.agenda)                                                                     
+                  }, 
+                  err => {
+                    console.log('err')         
+                  });    
+  };   
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpModule } from '@angular/http';
+import { OrderModule } from 'ngx-order-pipe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule, MatButtonModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
+
+import { AgendaService } from './services/agenda.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    OrderModule,
+  	MatButtonModule,
+  	MatTabsModule,
+  	BrowserAnimationsModule,
+  	HttpModule,
     BrowserModule
   ],
-  providers: [],
+  providers: [AgendaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
